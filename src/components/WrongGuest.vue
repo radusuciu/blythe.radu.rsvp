613a7f9d5111be82775a6e255faa0281fb05e7f0
Radu Suciu <radusuciu@gmail.com>
2021-12-28 07:56:44 -0800

    Use cuter face when guest is not found

--- src/components/WrongGuest.vue
+++ src/components/WrongGuest.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="notification is-warning is-light">
-        <strong>Sorry, we tried our best :/</strong>
+        <strong>Sorry, we tried our best (´･_･`)</strong>
         <p>You can keep typing your name (try it exactly like it was sent on your invite, for example: <em>John Doe</em>),
         or you can send us an email at: <a href="mailto:rsvp@blythe.radu.love">rsvp@blythe.radu.love</a>.</p>
     </div>
