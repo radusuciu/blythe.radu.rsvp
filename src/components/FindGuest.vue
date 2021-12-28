a1bd68ac2801dd25e68a1087bdb3001d6807a212
Radu Suciu <radusuciu@gmail.com>
2021-12-28 07:28:52 -0800

    Handle cases where we can't find a guest

--- src/components/FindGuest.vue
+++ src/components/FindGuest.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 
-import { ref, onMounted, computed, Ref } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue'
 import { useCachedRequest } from '../composables/useCachedRequest'
-import { findGuest, Guest, FindGuestAPIResponse } from '../api/guest'
+import { findGuest } from '../api/guest'
 import IdentityQuestion from '@/components/IdentityQuestion.vue'
+import WrongGuest from '@/components/WrongGuest.vue'
 import { useDebounce } from '@vueuse/core'
+import { watchOnce } from '@vueuse/core'
 
 
-defineEmits(['rightGuest', 'wrongGuest'])
+const emit = defineEmits(['rightGuest', 'wrongGuest'])
 
 const searchInput = ref<null | { focus: () => null }>(null)
 const searchTerm = ref('')
+const wrongGuest = ref(false)
 const debouncedTerm = useDebounce(searchTerm, 300)
 
 const {
@@ -21,43 +24,46 @@ const {
 } = useCachedRequest(debouncedTerm, findGuest)
 
 const debouncedIsLoading = useDebounce(isLoading, 300)
+// const debouncedIsLoading = isLoading.value
+const trimmedTerm = computed(() => searchTerm.value.trim())
+const termLength = computed(() => trimmedTerm.value.length)
 
 const isExactMatch = computed(() => {
     return error.value === undefined && data.value && data.value.guest && data.value.uniqueMatch
 })
 
 const isMultipleMatches = computed(() => {
-    return error.value === undefined && !data.value?.uniqueMatch && searchTerm.value.trim().length > 4 && data.value?.matches?.length
+    return error.value === undefined && !data.value?.uniqueMatch && termLength.value > 4 && data.value?.matches?.length
 })
 
 const keepTyping = computed(() => {
-    return error.value === undefined && !data.value?.uniqueMatch && searchTerm.value.trim().length > 2
+    return error.value === undefined && !data.value?.uniqueMatch && termLength.value > 2
 })
 
 const noMatches = computed(() => {
-    return error.value && !debouncedIsLoading.value && searchTerm.value.trim().length > 2
+    return error.value && !debouncedIsLoading.value && termLength.value > 2
 })
+
+
+function onWrongGuest() {
+    emit('wrongGuest')
+    wrongGuest.value = true
+    searchInput.value?.focus()
+    watchOnce(debouncedTerm, () => wrongGuest.value = false)
+}
 
 onMounted(() => {
     searchInput.value?.focus()
 })
-
-// TODO: if we had a unique match and the user keeps typing maybe stop making requests
-// or maybe just keep that last good match up if further input makes us unable to find
-// a good match
 
 </script>
 
 <template>
     <div>
         <h1 class="title">Hello lovely guest</h1>
-        <p class="subtitle">
-            Thank you for responding!
-        </p>
+        <p class="subtitle">Thank you for responding!</p>
         <div class="block">
-            <p class="">
-                First, let's find your invite, could we please have your name?
-            </p>
+            <p>First, let's find your invite, could we please have your name?</p>
             <div class="control is-medium" :class="{ 'is-loading': debouncedIsLoading }">
                 <input ref="searchInput" autofocus type="text" maxlength="100"
                     class="input is-primary is-medium"
@@ -71,43 +77,44 @@ onMounted(() => {
                 </p>
             </transition>
         </div>
-        <div class="block">
-            <div v-if="searchTerm.trim()">
-                <transition name="fade" mode="out-in">
-                    <IdentityQuestion
-                        v-if="isExactMatch"
-                        :disabled="debouncedIsLoading"
-                        :name="data?.guest?.name"
-                        :key="data!.guest!.id"
-                        @yes="$emit('rightGuest', data?.guest)"
-                        @no="$emit('wrongGuest')"
-                    />
-                    <div class="notification is-info is-light" v-else-if="isMultipleMatches">
-                        There are multiple guests matching the name <strong>{{ searchTerm.trim() }}</strong>, please keep typing if you can, or select yourself below:
+        <div class="block" v-if="termLength">
+            <transition name="fade" mode="out-in">
+                <WrongGuest v-if="wrongGuest" />
+                <IdentityQuestion
+                    v-else-if="isExactMatch"
+                    :disabled="debouncedIsLoading"
+                    :name="data?.guest?.name"
+                    :key="data!.guest!.id"
+                    @yes="$emit('rightGuest', data?.guest)"
+                    @no="onWrongGuest"
+                />
+                <div class="notification is-info is-light" v-else-if="isMultipleMatches">
+                    There are multiple guests matching the name <strong>{{ trimmedTerm }}</strong>, please keep typing if you can, or select yourself below:
 
-                        <div class="block field is-grouped mt-2">
-                            <span class="control" v-for="guest in data?.matches" :key="guest.id">
-                                <button @click="$emit('rightGuest', guest)" class="button is-multiline is-link is-outlined">
-                                    <strong>{{ guest.name }}</strong>
-                                    <div v-if="guest.party" v-for="partyGuest in guest.party" :key="partyGuest.id">
-                                        + {{ partyGuest.name }}
-                                    </div>
-                                </button>
-                            </span>
-                            <span class="control">
-                                <button class="button is-danger is-outlined is-multiline">None of these are me</button>
-                            </span>
-                        </div>
+                    <div class="block field is-grouped mt-2">
+                        <span class="control" v-for="guest in data?.matches" :key="guest.id">
+                            <button @click="$emit('rightGuest', guest)" class="button is-multiline is-link is-outlined">
+                                <strong>{{ guest.name }}</strong>
+                                <div v-if="guest.party" v-for="partyGuest in guest.party" :key="partyGuest.id">
+                                    + {{ partyGuest.name }}
+                                </div>
+                            </button>
+                        </span>
+                        <span class="control">
+                            <button class="button is-danger is-outlined is-multiline" @click="onWrongGuest">None of these are me</button>
+                        </span>
                     </div>
-                    <div class="notification is-info is-light" v-else-if="keepTyping">
-                        We could not find a good enough match for: <strong>{{ searchTerm.trim() }}</strong>, please keep typing.
-                    </div>
-                    <div class="notification is-danger is-light" v-else-if="noMatches">
-                        We couldn't find a guest matching the name <strong>{{ searchTerm.trim() }}</strong>. Please contact us at the following email and we'll figure it out: <a href="mailto:rsvp@blythe.radu.love">rsvp@blythe.radu.love</a>.
-                    </div>
-                </transition>
-            </div>
-            <p class=""></p>
+                </div>
+                <div class="notification is-info is-light" v-else-if="keepTyping">
+                    We could not find a good enough match for: <strong>{{ trimmedTerm }}</strong>, please keep typing.
+                    <p class="control has-text-centered mt-2" v-if="termLength > 4">
+                        <button class="button is-danger is-outlined" @click="onWrongGuest">I can't find myself!</button>
+                    </p>
+                </div>
+                <div class="notification is-danger is-light" v-else-if="noMatches">
+                    We couldn't find a guest matching the name <strong>{{ trimmedTerm }}</strong>. Please contact us at the following email and we'll figure it out: <a href="mailto:rsvp@blythe.radu.love">rsvp@blythe.radu.love</a>.
+                </div>
+            </transition>
         </div>
     </div>
 </template>
---
+++ src/components/WrongGuest.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="notification is-warning is-light">
+        <strong>Sorry, we tried our best :/</strong>
+        <p>You can keep typing your name (try it exactly like it was sent on your invite, for example: <em>John Doe</em>),
+        or you can send us an email at: <a href="mailto:rsvp@blythe.radu.love">rsvp@blythe.radu.love</a>.</p>
+    </div>
+</template>
